85c2eb7c1056b4bc7095643ffa47538fcb1e6a97
Qingyi Dong <dqyfootball@icloud.com>
2019-08-23 12:19:45 -0700

    updated menu-items to nevigate withRoauter Higher Order Component (HOC)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const NewPage = () => (
+  <div>
+    <h1>NEW</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path = '/' component = { HomePage }/>
+        <Route exact path = '/new' component = { NewPage }/>
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,32 +13,32 @@ class Directory extends React.Component {
               title: '當季 | New',
               imageUrl: 'https://i.ibb.co/nBM6TSK/6-D28-FC89-8-A94-4-ED3-B59-C-53-B4500-CD6-DE.jpg',
               id: 1,
-              linkUrl: 'shop/new'
+              linkUrl: 'new'
             },
             {
               title: '街頭 | Street',
               imageUrl: 'https://i.ibb.co/q5XMsH7/1-A16-C4-D5-FB49-46-E2-8665-EF95-C23-EEB1-A.jpg',
               id: 2,
-              linkUrl: 'shop/street'
+              linkUrl: 'street'
             },
             {
               title: '都市 | Urban',
               imageUrl: 'https://i.ibb.co/09M07dJ/3-DD9-E8-D2-9094-4185-AB44-5-D2-C5726046-F.jpg',
               id: 3,
-              linkUrl: 'shop/urban'
+              linkUrl: 'urban'
             },
             {
               title: '首創 | Origin',
               imageUrl: 'https://i.ibb.co/VJpnmcx/IMG-7996.jpg',
               id: 4,
-              linkUrl: 'shop/urban'
+              linkUrl: 'origin'
             },
             {
               title: 'Big WHhite Tote ©',
               imageUrl: 'https://i.ibb.co/31LWR68/BWTCOVER.jpg',
               id: 5,
               size: 'large',
-              linkUrl: 'shop/all'
+              linkUrl: 'all'
             }]
         };
     };
@@ -47,8 +47,8 @@ class Directory extends React.Component {
         return (
             <div className = 'directory-menu'>
                 {
-                    this.state.sections.map(({ title, imageUrl, id, linkUrl, size }) => (
-                    <MenuItem key = {id} title = {title} imageUrl = {imageUrl} size= {size} />
+                    this.state.sections.map(({ id, ...otherSectionProps }) => (
+                    <MenuItem key = {id} { ...otherSectionProps } />
                     ))
                 }
             </div>
